e8803004166506ba1b1e3e1e8a497dc3883d37a9
huning <huning@163.com>
2022-09-07 22:01:08 +0800

    胡 2022.09.07 第二次提交，分类页静态结构基本完成，提交备注写错过，这是修改后的备注

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,80 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
+// pages/index/index.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    aside:["侧边栏","侧边栏","侧边栏","侧边栏","侧边栏","侧边栏","侧边栏","侧边栏","侧边栏","侧边栏","侧边栏","侧边栏","侧边栏","侧边栏","侧边栏"],
+    produce:[
+      {type:"家电",goods:[
+        {pname:"冰箱", img:"../../img/index.png"},
+        {pname:"冰箱", img:"../../img/index.png"},
+        {pname:"冰箱", img:"../../img/index.png"},
+        {pname:"冰箱", img:"../../img/index.png"},
+      ]},
+      {type:"数码",goods:[
+        {pname:"手机", img:"../../img/index.png"},
+        {pname:"手机", img:"../../img/index.png"},
+        {pname:"手机", img:"../../img/index.png"},
+        {pname:"手机", img:"../../img/index.png"},
+      ]},
+    ],
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  // "侧边栏","侧边栏","侧边栏","侧边栏",
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
   },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
   }
 })
